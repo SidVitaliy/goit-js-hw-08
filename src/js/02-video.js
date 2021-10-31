46c1d461e6da57cc1cdeaccb2b7e1f7bbacad962
Vitaliy <sivittat@gmail.com>
2021-10-31 11:55:35 +0200

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,19 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+
+
 const player = new Player('vimeo-player', {
     id: 236203659,
     width: 640
 });
-localStorage.setItem('videoplayer-current-time', time)
 
-player.on('timeupdate', function (data) {
-        // data is an object containing properties specific to that event
-    });
+function onCurrentTime(event) {
+    localStorage.setItem('videoplayer-current-time', `${event.seconds}`)
+};
 
+player.on('timeupdate', throttle(onCurrentTime, 500));
 
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
